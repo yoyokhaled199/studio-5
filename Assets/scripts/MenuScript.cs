738b9b9953c4cbd986b062a2544f4a02059daf6e
karimaboulenein <karim@largelabs.fr>
2025-05-15 16:05:42 +0300

    Code review

--- Assets/scripts/MenuScript.cs
+++ Assets/scripts/MenuScript.cs
@@ -4,6 +4,9 @@ using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
+    // Code review : add some UI to choose your character type
+    // And find a way to save it or make it persistent 
+    // so that you can retrieve the proper config when the level is loaded
 
 
     public void PlayGame()
---
+++ Assets/scripts/PlayerConfig.cs
@@ -0,0 +1,12 @@
+
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerConfig", menuName = "PlayerConfig", order = 0)]
+public class PlayerConfig : ScriptableObject 
+{
+    public Sprite middleSprite;
+    public Sprite rightSprite;
+    public Sprite leftSprite;
+}
+
